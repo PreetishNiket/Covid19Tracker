d3e533b911ec782ff0da4793a3e391cd5a64fbc4
Preetish Niket <niketpreetish@gmail.com>
2021-05-24 12:30:29 +0530

    SplashScreenUpdated

--- app/src/main/java/com/example/covid19tracker/SplashScreenActivity.kt
+++ app/src/main/java/com/example/covid19tracker/SplashScreenActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

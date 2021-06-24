138aad8a01b04f31120390997e8e2f5fdf1c84d6
niketpreetish <niketpreetish@gmail.com>
2021-06-24 22:47:43 +0530

    MainScreen UI Updated

--- app/src/main/java/com/example/covid19tracker/MainActivity.kt
+++ app/src/main/java/com/example/covid19tracker/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.covid19tracker.networking.Client
 import com.google.gson.Gson
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -25,110 +26,17 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var stateListAdapter: StateListAdapter
-    var isOpen = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        val fabOpen = AnimationUtils.loadAnimation(
-            this,
-            R.anim.fab_open
-        )
-        val fabClose = AnimationUtils.loadAnimation(
-            this,
-            R.anim.fab_close
-        )
-        val fabRClockwise = AnimationUtils.loadAnimation(
-            this,
-            R.anim.rotate_clockwise
-        )
-        val fabRAntiClockwise = AnimationUtils.loadAnimation(
-            this,
-            R.anim.rotate_anticlockwise
-        )
-        Tracker_fab.setOnClickListener {
-            if(isOpen){
-                About_fab.startAnimation(fabClose)
-                Symptoms_fab.startAnimation(fabClose)
-                Essentials_fab.startAnimation(fabClose)
-                Prevention_fab.startAnimation(fabClose)
-                Vaccine_fab.startAnimation(fabClose)
-
-                //visibility//
-                About_fab.visibility = View.INVISIBLE
-                Symptoms_fab.visibility = View.INVISIBLE
-                Essentials_fab.visibility = View.INVISIBLE
-                Prevention_fab.visibility = View.INVISIBLE
-                Vaccine_fab.visibility=View.INVISIBLE
-                //clickable
-
-                About_fab.isClickable = false
-                Symptoms_fab.isClickable = false
-                Essentials_fab.isClickable = false
-                Prevention_fab.isClickable = false
-                Vaccine_fab.isClickable=false
-                //disabling when fab close
-
-                About_fab.isEnabled = false
-                Symptoms_fab.isEnabled = false
-                Essentials_fab.isEnabled = false
-                Prevention_fab.isEnabled = false
-                Vaccine_fab.isEnabled=false
-
-                Tracker_fab.startAnimation(fabRClockwise)
-                isOpen = false
-
-            }else{
-                About_fab.startAnimation(fabOpen)
-                Symptoms_fab.startAnimation(fabOpen)
-                Essentials_fab.startAnimation(fabOpen)
-                Prevention_fab.startAnimation(fabOpen)
-                Vaccine_fab.startAnimation(fabOpen)
-                Tracker_fab.startAnimation(fabRAntiClockwise)
-                ////
-
-                About_fab.visibility = View.VISIBLE
-                Symptoms_fab.visibility = View.VISIBLE
-                Essentials_fab.visibility = View.VISIBLE
-                Prevention_fab.visibility = View.VISIBLE
-                Vaccine_fab.visibility = View.VISIBLE
-                //
-
-                About_fab.isClickable = true
-                Symptoms_fab.isClickable = true
-                Essentials_fab.isClickable = true
-                Prevention_fab.isClickable = true
-                Vaccine_fab.isClickable=true
-
-                ///////////
-                About_fab.isEnabled = true
-                Symptoms_fab.isEnabled = true
-                Essentials_fab.isEnabled = true
-                Prevention_fab.isEnabled = true
-                Vaccine_fab.isEnabled=true
-                isOpen = true
-            }
-            About_fab.setOnClickListener {
-//                val intent = Intent(this, AboutActivity::class.java)
-//                startActivity(intent)
-            }
-            Symptoms_fab.setOnClickListener {
-//                val intent = Intent(this, SymptomActivity::class.java)
-//                startActivity(intent)
-            }
-            Essentials_fab.setOnClickListener {
-//                val intent = Intent(this, EssentialActivity::class.java)
-//                startActivity(intent)
-            }
-            Prevention_fab.setOnClickListener {
-//                val intent = Intent(this, PreventionActivity::class.java)
-//                startActivity(intent)
-            }
-            Vaccine_fab.setOnClickListener {
-                startActivity(Intent(this,VaccineActivity::class.java))
-            }
-
+        vaccine_btn.setOnClickListener {
+            startActivity(Intent(this,VaccineActivity::class.java))
+        }
+        facts.setOnClickListener {
+//            startActivity(Intent(this,FactsActivity::class.java))
         }
         list.addHeaderView(
             LayoutInflater.from(this)
